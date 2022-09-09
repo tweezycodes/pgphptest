3cbf8c7a4471a653c25ed2fb507541b63c5f3435
Basit <ab.umparsalic@gmail.com>
2022-09-09 13:29:03 +0800

    added models

--- database/migrations/2022_09_09_052228_create_comments_table.php
+++ database/migrations/2022_09_09_052228_create_comments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->text('comments');
+            $table->text('comment');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
